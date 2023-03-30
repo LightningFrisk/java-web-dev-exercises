8254f64359b663311ac5c4caf4aa59e27ea7e021
LightningFrisk <bmxcaleb@gmail.com>
2023-03-30 17:47:09 -0500

    first pass of exercisees

---
+++ src/exercises/ArrayListPractice.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class ArrayListPractice {
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
